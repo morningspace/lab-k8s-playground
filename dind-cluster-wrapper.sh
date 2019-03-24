be4da4bcab9e62de1066416272239c9b6d209ad3
moyingbj <moyingbj@cn.ibm.com>
2019-03-24 20:28:46 +0800

    Modify env handling code

--- dind-cluster-wrapper.sh
+++ dind-cluster-wrapper.sh
@@ -1,77 +1,77 @@
 #!/bin/bash
 
-# Kubernetes version
-DIND_K8S_VERSION=${DIND_K8S_VERSION:-v1.13}
-DIND_COMMIT=${DIND_COMMIT:-76f7b8a5f3966aa80700a8c9f92d23f6936f949b}
+env_vars=()
 
-# Build Kubernetes from source
-BUILD_KUBEADM=${BUILD_KUBEADM:-}
-BUILD_HYPERKUBE=${BUILD_HYPERKUBE:-}
-
-# Customize Kubernetes Dashboard URL
-DASHBOARD_BASE_URL="https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy"
-DASHBOARD_URL="${DASHBOARD_BASE_URL}/recommended/kubernetes-dashboard.yaml"
-# DASHBOARD_URL="${DASHBOARD_BASE_URL}/alternative/kubernetes-dashboard.yaml"
-# To skip Kubernetes Dashboard deployment
-SKIP_DASHBOARD=${SKIP_DASHBOARD:-1}
-
-# To have kubeadm-dind-cluster join custom networks, separated by comma
-DIND_CUSTOM_NETWORKS=${DIND_CUSTOM_NETWORKS:-net-registry}
-# To use insecure private Docker registries for kubeadm to pull images from there
-DIND_INSECURE_REGISTRIES=${DIND_INSECURE_REGISTRIES:-'["k8s.gcr.io", "gcr.io", "mirantis", "mr.io"]'}
-
-# To skip pull of image kubeadm-dind-cluster
-DIND_SKIP_PULL=1
-
-# To skip snapshot
-# SKIP_SNAPSHOT=1
-
-#################################################
-
-function log_env() {
-  echo "DIND_K8S_VERSION=$DIND_K8S_VERSION"
-  echo "BUILD_KUBEADM=$BUILD_KUBEADM"
-  echo "BUILD_HYPERKUBE=$BUILD_HYPERKUBE"
-  echo "DASHBOARD_URL=$DASHBOARD_URL"
-  echo "SKIP_DASHBOARD=$SKIP_DASHBOARD"
-  echo "DIND_CUSTOM_NETWORKS=$DIND_CUSTOM_NETWORKS"
-  echo "DIND_INSECURE_REGISTRIES=$DIND_INSECURE_REGISTRIES"
-  echo "DIND_SKIP_PULL=$DIND_SKIP_PULL"
-  echo "SKIP_SNAPSHOT=$SKIP_SNAPSHOT"
+function env() {
+  env_vars+=($1)
+  eval "$1=\${$1:-$2}"
 }
 
-SCRIPT_HOME=$(cd -P "$(dirname "$0")" && pwd)
-SCRIPT_BASEURL="https://raw.githubusercontent.com/morningspace/kubeadm-dind-cluster"
+function envs() {
+  for env_var in "${env_vars[@]}" ; do
+    eval "echo $env_var=\$$env_var"
+  done
+}
 
-function load_script() {
-  local script_name
-  for script_name in "$@" ; do
-    local script_dir="$SCRIPT_HOME/$script_name"
-    local script_url="$SCRIPT_BASEURL/master/$script_name"
+script_home=$(cd -P "$(dirname "$0")" && pwd)
+script_name="dind-cluster.sh"
+script_dir="$script_home/$script_name"
+script_baseurl="https://raw.githubusercontent.com/morningspace/kubeadm-dind-cluster"
 
-    if [ ! -f $script_dir ] ; then
-      echo "Download $script_name ..."
-      curl -sSL $script_url > $script_dir
+function install_script() {
+  local script force=$1
+  for script in "$script_name" "config.sh" ; do
+    local script_dir="$script_home/$script"
+    local script_url="$script_baseurl/master/$script"
+    if [[ ! -f $script_dir || -n $force ]] ; then
+      echo "Download $script ..."
+      curl -sSLO $script_url
+      [ $? != 0 ] && exit 1
       chmod +x $script_dir
     fi
   done
 }
 
 function run_script() {
-  local script_name="dind-cluster.sh"
-  load_script $script_name "config.sh"
-
-  local script_dir="$SCRIPT_HOME/$script_name"
-
-  echo "Run $script_name ..."
   start_time=$SECONDS
   . $script_dir
   elapsed_time=$(($SECONDS - $start_time))
   echo "Total elapsed time: $elapsed_time seconds"
 }
 
+#################################################
+
+# Kubernetes version
+env DIND_K8S_VERSION v1.13
+env DIND_COMMIT 76f7b8a5f3966aa80700a8c9f92d23f6936f949b
+
+# Build Kubernetes from source
+env BUILD_KUBEADM
+env BUILD_HYPERKUBE
+
+# Customize Kubernetes Dashboard
+DASHBOARD_BASEURL="https://raw.githubusercontent.com/kubernetes/dashboard/master/aio/deploy"
+env DASHBOARD_URL ${DASHBOARD_BASEURL}/recommended/kubernetes-dashboard.yaml
+# env DASHBOARD_URL ${DASHBOARD_BASEURL}/alternative/kubernetes-dashboard.yaml
+env SKIP_DASHBOARD 1
+
+# To have kubeadm-dind-cluster join custom networks, separated by comma
+env DIND_CUSTOM_NETWORKS net-registry
+# To use insecure private Docker registries for kubeadm to pull images from there
+env DIND_INSECURE_REGISTRIES '"[\"k8s.gcr.io\", \"gcr.io\", \"mirantis\", \"mr.io\"]"'
+
+# To skip pull of image kubeadm-dind-cluster
+env DIND_SKIP_PULL 1
+# To skip snapshot
+env SKIP_SNAPSHOT
+
+#################################################
+
 if [[ $1 == 'env' ]] ; then
-  log_env
+  envs
+elif [[ $1 == 'install' ]] ; then
+  install_script 1
 else
+  install_script
   run_script $@
 fi
