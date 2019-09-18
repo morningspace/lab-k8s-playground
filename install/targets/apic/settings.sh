05c25ac68ba4e88b31f348bd394641efe0482be6
morningspace <morningspace@yahoo.com>
2019-09-18 11:00:16 +0800

    Use the single apic_domain to configure all apic subsystems

--- install/targets/apic/settings.sh
+++ install/targets/apic/settings.sh
@@ -2,20 +2,21 @@
 apic_ns=apiconnect
 apic_skip_load_images=
 max_map_count=262144
+apic_domain=${apic_domain:-morningspace.com}
 
 # management subsystem
-platform_api=api.morningspace.com
-api_manager_ui=apim.morningspace.com
-cloud_admin_ui=cm.morningspace.com
-consumer_api=consumer.morningspace.com
+platform_api=api.$apic_domain
+api_manager_ui=apim.$apic_domain
+cloud_admin_ui=cm.$apic_domain
+consumer_api=consumer.$apic_domain
 # cassandra_max_memory_gb=2
 cassandra_max_memory_gb=4
 # cassandra_volume_size_gb=1
 cassandra_volume_size_gb=5
 
 # gateway subsystem
-api_gateway=gw.morningspace.com
-apic_gw_service=gwd.morningspace.com
+api_gateway=gw.$apic_domain
+apic_gw_service=gwd.$apic_domain
 image_repository=datapower-api-gateway
 image_tag=2018.4.1.4-307525-release
 # tms_peering_storage_size_gb=2
@@ -24,8 +25,8 @@ tms_peering_storage_size_gb=5
 max_memory_gb=6
 
 # analytics subsystem
-analytics_ingestion=ai.morningspace.com
-analytics_client=ac.morningspace.com
+analytics_ingestion=ai.$apic_domain
+analytics_client=ac.$apic_domain
 # coordinating_max_memory_gb=1
 coordinating_max_memory_gb=2
 # data_max_memory_gb=1
@@ -37,8 +38,8 @@ master_max_memory_gb=2
 master_storage_size_gb=5
 
 # portal subsystem
-portal_admin=padmin.morningspace.com
-portal_www=portal.morningspace.com
+portal_admin=padmin.$apic_domain
+portal_www=portal.$apic_domain
 # www_storage_size_gb=1
 www_storage_size_gb=5
 # backup_storage_size_gb=1
