7830296606e27bb4842b9a1e1b165888a8175796
morningspace <morningspace@yahoo.com>
2019-10-09 14:59:35 +0800

    Do not call setcap if os is darwin

--- install/targets/kubectl.sh
+++ install/targets/kubectl.sh
@@ -38,4 +38,6 @@ if [[ ! -f ~/.launch-cache/$executable ]]; then
 fi
 
 create_links ~/.launch-cache/$executable kubectl
-sudo setcap CAP_NET_BIND_SERVICE=+ep ~/.launch-cache/$executable
+if [[ $os != darwin ]]; then
+  sudo setcap CAP_NET_BIND_SERVICE=+ep ~/.launch-cache/$executable
+fi
